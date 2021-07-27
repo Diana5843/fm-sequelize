57899ff906bb2073d604c11bf94e9fab6ad863d5
Diana Sokolova <dianasokolova5843@gmail.com>
2021-07-27 09:01:27 +0300

    init commit

---
+++ models/user.js
@@ -0,0 +1,76 @@
+'use strict';
+const { Model } = require('sequelize');
+const { isAfter } = require('date-fns');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      User.hasMany(models.Task, {
+        foreignKey: 'userId',
+      });
+    }
+  }
+  User.init(
+    {
+      firstName: {
+        field: 'first_name',
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      lastName: {
+        field: 'last_name',
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+          isEmail: true,
+        },
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      birthday: {
+        type: DataTypes.DATEONLY,
+        validate: {
+          isDate: true,
+          isValidDate (value) {
+            if (isAfter(new Date(value), new Date())) {
+              throw new Error('Check your birthday, man');
+            }
+          },
+        },
+      },
+      isMale: {
+        field: 'is_male',
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+      underscored: true,
+    }
+  );
+  return User;
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const app = require('./app');
+
+/*
+  СЛУШАТЬ ПОРТ
+  РАБОТА С ЗАПРОСАМИ (СЛУШАТЬ/ОТВЕЧАТЬ)
+  РАБОТА С БД
+*/
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
+
+/*
+  Model: Task
+  userId:INTEGER NOT NULL
+  body:VARCHAR(512) NOT NULL, != ''
+  isDone:BOOLEAN NOT NULL, DEFAULT false
+  deadline:DATE
+  npx sequelize-cli model:generate --name User --attributes firstName:string,lastName:string,email:string
+*/
